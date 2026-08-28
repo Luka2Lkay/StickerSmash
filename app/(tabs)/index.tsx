21aa6d2ed562a3974dbbc6282a14e9137fbbf3c3
Luka <luka.matshebelele@gmail.com>
2026-08-28 17:02:08 +0200

    add navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home Screen</Text>
+      <Link href={"/about"} style={styles.button}>Go to About screen</Link>
     </View>
   );
 }
@@ -19,5 +21,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#fff"
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff'
   }
 })
