bed62fc43647f9522038157361a4f2f2117e6dd9
Luka <luka.matshebelele@gmail.com>
2026-09-07 10:43:05 +0200

    add gestures

--- components/emoji-sticker.tsx
+++ components/emoji-sticker.tsx
@@ -1,4 +1,4 @@
-import { ImageSourcePropType, View } from "react-native";
+import { ImageSourcePropType } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 
@@ -9,6 +9,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     const scaleImage = useSharedValue(imageSize)
+    const translateX = useSharedValue(0)
+    const translateY = useSharedValue(0)
 
     const doubleTap = Gesture.Tap().numberOfTaps(2).onStart(() => {
         if (scaleImage.value !== imageSize * 2) {
@@ -24,13 +26,28 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
             height: withSpring(scaleImage.value)
         }
     })
-    
-    return (
-        <View style={{ top: -350 }}>
-            <GestureDetector gesture={doubleTap}>
-                <Animated.Image source={stickerSource} resizeMode="contain" style={imageStyle} />
-            </GestureDetector>
 
-        </View>
+    const drag = Gesture.Pan().onChange((event) => {
+        translateX.value += event.changeX
+        translateY.value += event.changeY
+    })
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: translateX.value },
+                { translateY: translateY.value }
+            ]
+        }
+    })
+
+    return (
+        <GestureDetector gesture={drag}>
+            <Animated.View style={[{ top: -350 }, containerStyle]}>
+                <GestureDetector gesture={doubleTap}>
+                    <Animated.Image source={stickerSource} resizeMode="contain" style={imageStyle} />
+                </GestureDetector>
+            </Animated.View>
+        </GestureDetector>
     )
 }
